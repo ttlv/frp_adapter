85cd3698ebc4a7d6d019bf4eacae23662eb6866f
ttlv <gogophersteven@gmail.com>
2021-04-25 17:37:19 +0800

    fix/notify: remoe log.Fatal

--- go.mod
+++ go.mod
@@ -12,13 +12,11 @@ require (
 	github.com/mitchellh/mapstructure v1.1.2
 	github.com/royeo/dingrobot v1.0.0
 	github.com/sirupsen/logrus v1.6.0
-	github.com/smartystreets/goconvey v1.6.4 // indirect
 	github.com/tidwall/gjson v1.6.1
 	github.com/ttlv/common_utils v0.0.0-20200924063058-d95c564b1b7b
 	github.com/ttlv/nodemaintenances v0.0.0-20210407033341-8e1c2cad7c92
 	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
-	gopkg.in/ini.v1 v1.62.0
 	k8s.io/apimachinery v0.17.2
 	k8s.io/client-go v0.17.2
 	k8s.io/utils v0.0.0-20200912215256-4140de9c8800 // indirect
--- notification/notification.go
+++ notification/notification.go
@@ -1,16 +1,15 @@
 package notification
 
 import (
-	"log"
-
 	"github.com/gin-gonic/gin"
 	"github.com/hoisie/mustache"
 	"github.com/royeo/dingrobot"
+	"github.com/sirupsen/logrus"
 	"github.com/ttlv/frp_adapter/config"
 )
 
 func Notice(operating string, status_now string, c *gin.Context) {
-	c := config.MustGetFrpsConfig()
+	cfg := config.MustGetFrpsConfig()
 
 	var str = map[string]interface{}{"uid": c.Request.FormValue("unique_id"), "status_now": status_now, "operating": operating, "status": c.Request.FormValue("status")}
 	content := "####  节点{{operating}}  \n>  节点{{uid}}状态由{{status_now}} 变更为{{status}} ![screenshot](https://z3.ax1x.com/2021/04/17/c4XKds.png)\n> ###### 发布from  [谐云](http://www.harmonycloud.cn/overindex) \n"
@@ -18,14 +17,12 @@ func Notice(operating string, status_now string, c *gin.Context) {
 	text := mustache.Render(content, str)
 
 	//生成dingding机器人通讯
-	robot := dingrobot.NewRobot(c.Webhook)
+	robot := dingrobot.NewRobot(cfg.Webhook)
 
 	atMobiles := []string{""}
 	isAtAll := true
 	title := "节点创建更新通知"
-	err = robot.SendMarkdown(title, text, atMobiles, isAtAll)
-	if err != nil {
-		log.Fatal(err)
+	if err := robot.SendMarkdown(title, text, atMobiles, isAtAll); err != nil {
+		logrus.Error(err)
 	}
-
 }
